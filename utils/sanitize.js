3daab3011bb6943ee8575b89ee32e409ecea381c
Herashini Raman <herashini.raman@qiu.edu.my>
2026-03-07 03:45:36 +0800

    Enhance input sanitization and basic XSS prevention

--- utils/sanitize.js
+++ utils/sanitize.js
@@ -1,9 +1,11 @@
 function sanitizeText(value) {
   if (typeof value !== "string") return "";
+
   return value
     .trim()
-    .replace(/[<>]/g, "")
-    .replace(/[\u0000-\u001F\u007F]/g, "");
+    .replace(/[<>]/g, "")        // remove HTML tags
+    .replace(/["']/g, "")        // remove quotes
+    .replace(/[\u0000-\u001F]/g, ""); // remove control characters
 }
 
 module.exports = { sanitizeText };
